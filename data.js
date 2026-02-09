b6c3ec6644d8874c1654099becdf4ab7d0ecf767
Vinayvk-3d <vkvinaymaniyur@gmail.com>
2026-02-09 22:25:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living",
+      "name": "LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.17957514790564488,
+        "pitch": -0.1318473601870167,
+        "fov": 0.5755413909353084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.36858243640547883,
+          "pitch": -0.0059452832867172845,
+          "rotation": 0,
+          "target": "0-living"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "LIVING",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
